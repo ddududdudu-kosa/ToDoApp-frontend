ed0ca218a3743bb2b57c1ef19d2d190ca3351d1d
Kimjonghyeon6084 <ssoert123@naver.com>
2024-07-30 21:53:31 +0900

    feat: feed ui 50% 완성

---
+++ src/views/feed/FeedMyInfoCompo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img :src="user.profileImage" :alt="user.name" class="profile-image"/>
+        <!-- <span class="user-name">{{ user.name }}</span> -->
+        <span class="user-name">내정보인데용?</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+        users: [
+            { id: 1, name: 'JohnDoe', profileImage: 'path/to/image1.jpg' },
+        ]
+    }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/feed/FeedStroyCompo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="story-container">
+        <div class="story" v-for="user in users" :key="user.id">
+            <img :src="user.profileImage" :alt="user.name" class="profile-image"/>
+            <span class="user-name">{{ user.name }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+        users: [
+            { id: 1, name: 'JohnDoe', profileImage: 'path/to/image1.jpg' },
+            { id: 2, name: 'JaneDoe', profileImage: 'path/to/image2.jpg' },
+            { id: 2, name: 'JaneDoe', profileImage: 'path/to/image2.jpg' },
+            { id: 2, name: 'JaneDoe', profileImage: 'path/to/image2.jpg' },
+        ]
+    }
+    }
+}
+</script>
+
+<style scoped>
+    @import "@/assets/feed.css"
+</style>
